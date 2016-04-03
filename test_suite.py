836186377f2ce82e855648b07f5cec7bef81cbf1
Alex Lederer <ARL94@pitt.edu>
2016-04-03 17:04:14 -0400

    Generalized test suite to accommodate multiple nlp models.

--- test_suite.py
+++ test_suite.py
@@ -1,7 +1,16 @@
-#importing classes
+# Importing classes
 from baseline import BaseLine
 from review import Review
+import argparse
 import json
+import sys
+
+# Create command line arguments.
+parser = argparse.ArgumentParser(description="Uses NLP models to predict the season of a Yelp review.") # TODO: revise so-called project title.
+parser.add_argument('-m', required=True, default="baseline", help="the NLP model to be used", action="store", dest="model")
+#parser.add_argument('-r', '-train', required = True, help = "name of training file", action = 'store', dest='train_file')
+#parser.add_argument('-t', '-test', required = True, help = "name of test file", action = 'store', dest="test_file")
+args = parser.parse_args()
 
 reviews = [] #  creating a list of reviews to classify
 
@@ -27,11 +36,15 @@ with open("winter-pittsburgh-training.json") as json_file:
     reviews += [('winter',json_obj)]
 
 #  Creating model objects
-base_obj = BaseLine(reviews)
-baseline_classified = [] #  classifications stored here
+model = args.model
+if (model == "baseline"):
+  model_obj = BaseLine(reviews)
+else: # put additional models here.
+  print "Argument Error: invalid model specified"
+  sys.exit()
 
+model_classified = [] #  classifications stored here
 reviews = [] #  resetting reviews list to save memory
-
 
 #  Reading test data into reviews list
 with open("spring-pittsburgh.json") as json_file:
@@ -55,18 +68,17 @@ with open("winter-pittsburgh.json") as json_file:
     reviews += [('winter',json_obj)]
 
 for __, json_obj in reviews:
-  baseline_classified += [(base_obj.classify(json_obj['text']), json_obj)]
+  model_classified += [(model_obj.classify(json_obj['text']), json_obj)]
 
 correct = 0
 index = 0
 
-#Going through every classification baseline made
-for classification in baseline_classified:
-  #if the tuple the baseline predicted is correct...
+# Going through every classification baseline made
+for classification in model_classified:
+  # if the tuple the baseline predicted is correct...
   if classification == reviews[index]:
     correct = correct + 1
   index = index + 1
 
-#print accuracy
-print (float(correct) / len(reviews))
+print (float(correct) / len(reviews)) # print accuracy
 
