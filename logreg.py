f7c6be7b0f41ffdf827f1caf32812ae361c208a4
Colin Swan <colin.swan@pitt.edu>
2016-04-08 00:09:26 -0400

    Added some documentation

--- logreg.py
+++ logreg.py
@@ -1,3 +1,9 @@
+"""
+Author: Colin Swan
+Description: Logistic Regression based classification of Yelp Reviews,
+categorizing reviews into seasons.
+
+"""
 import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -5,26 +11,30 @@ from sklearn.linear_model import LogisticRegression
 class LogReg:
 
   def __init__(self, reviews):
-    #Ignoring the reviews for baseline, but in actual models use
-    #the (category, review_json) tuples in the reviews list to train the model!
     corpus = []
     labels = []
     for review in reviews:
       corpus += [review[1]["text"]]
       labels += [review[0]]
 
+    #setting variables for the object
     self.corpus = corpus
     self.labels = labels
 
     self.reviews = reviews
+    #calling this object's train method
     self.train()
 
   def train(self):
+    #Vectorizing using TfidfVectorizer, which takes a count of all the words
+    #and then does some extra work to eliminate ones that are too common among
+    #all labels to be worth while.
     vectorizer = TfidfVectorizer(min_df = 1)
     X = vectorizer.fit_transform(self.corpus)
     y = self.labels
 
     model = LogisticRegression()
+    #Training the model
     model.fit(X, y)
     self.model = model
 
@@ -39,6 +49,7 @@ class LogReg:
 
     return 'summer'
 
+  #This is mainly a test method while I work on some implementation details
   def classify_all(self, all_test_data):
     test_corpus = []
     y = []
