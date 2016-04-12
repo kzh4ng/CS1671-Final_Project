f2a03f7f2e1d222851ca6c7313678d88c6beb153
Alex Lederer <ARL94@pitt.edu>
2016-04-12 09:08:12 -0400

    Bug fixes, new datasets.

--- parse_data.py
+++ parse_data.py
@@ -73,6 +73,29 @@ def get_city_list():
 	for c in city_dict.keys(): 
 		cities.write(c) 
 
+def print_city_list():
+	places = []
+	city_dict = {}
+	with open('yelp_academic_dataset_business.json') as f:
+		for line in f:
+			places.append(json.loads(line))
+
+	city_dict = dict()
+	for p in places:
+		city = p['city']
+		if city in city_dict:
+			city_dict[city] += 1
+		else: # new city
+			city_dict[city] = 1
+
+	city_dict_view = [ (v,k) for k,v in city_dict.iteritems() ]
+	city_dict_view.sort(reverse=True) # natively sort tuples by first element
+	for v,k in city_dict_view:
+		print "%s: %d" % (k,v)
+
+	#for c in city_dict:
+	#	print c + ": " + str(city_dict[c])
+
 def parse_by_city(city):
 	business_list = []
 	with open('yelp_academic_dataset_business.json') as f:
@@ -132,16 +155,10 @@ def sort_reviews_by_season(reviews, city):
 		else: # month in fallmonths
 			fall.append(r)
 
-	winterfile = open("winter-" + city + ".txt", "w")
-	springfile = open("spring-" + city + ".txt", "w")
-	summerfile = open("summer-" + city + ".txt", "w")
-	fallfile = open("fall-" + city + ".txt", "w")
-
 	print len(winter)
 	print len(spring)
 	print len(summer)
 	print len(fall)
-
 
 	winterfile2 = open("winter-" + city + ".json", "w")
 	springfile2 = open("spring-" + city + ".json", "w")
@@ -154,44 +171,98 @@ def sort_reviews_by_season(reviews, city):
 	fallfile3 = open("fall-" + city + "-training.json", "w")
 
 	for item in range(0, len(winter)/20):
-		winterfile.write(str(winter[item]) + "\n")
-		json.dump(winter[item], winterfile2)
-		winterfile2.write("\n")
 		if item % 20 == 0:
 			json.dump(winter[item], winterfile3)
 			winterfile3.write("\n")
+		else:
+			#winterfile.write(str(winter[item]) + "\n")
+			json.dump(winter[item], winterfile2)
+			winterfile2.write("\n")
 
 	for item in range(0, len(spring)/20):
-		springfile.write(str(spring[item]) + "\n")
-		json.dump(spring[item], springfile2)
-		springfile2.write("\n")
 		if item % 20 == 0:
 			json.dump(spring[item], springfile3)
 			springfile3.write("\n")
+		else:
+			#springfile.write(str(spring[item]) + "\n")
+			json.dump(spring[item], springfile2)
+			springfile2.write("\n")
+		
 
 	for item in range(0, len(summer)/20):
-		summerfile.write(str(summer[item]) + "\n")		
-		json.dump(summer[item], summerfile2)
-		summerfile2.write("\n")
 		if item % 20 == 0:
 			json.dump(summer[item], summerfile3)
 			summerfile3.write("\n")
-
+		else:
+			#summerfile.write(str(summer[item]) + "\n")		
+			json.dump(summer[item], summerfile2)
+			summerfile2.write("\n")
+		
 	for item in range(0, len(fall)/20):
-		fallfile.write(str(fall[item]) + "\n")
-		json.dump(fall[item], fallfile2)
-		fallfile2.write("\n")
 		if item % 20 == 0:
 			json.dump(fall[item], fallfile3)
 			fallfile3.write("\n")
+		else:
+			#fallfile.write(str(fall[item]) + "\n")
+			json.dump(fall[item], fallfile2)
+			fallfile2.write("\n")
+		
 
 # Generate four text files, by season, of reviews for a specific city.
+'''
 pittsburgh_business_list = parse_by_city("Pittsburgh")
 print len(pittsburgh_business_list)
 pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
 print len(pittsburgh_reviews)
 sort_reviews_by_season(pittsburgh_reviews, "pittsburgh")
 
+print "Vegas"
+pittsburgh_business_list = parse_by_city("Las Vegas")
+print len(pittsburgh_business_list)
+pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
+print len(pittsburgh_reviews)
+sort_reviews_by_season(pittsburgh_reviews, "lasvegas")
+
+print "Phoenix"
+pittsburgh_business_list = parse_by_city("Phoenix")
+print len(pittsburgh_business_list)
+pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
+print len(pittsburgh_reviews)
+sort_reviews_by_season(pittsburgh_reviews, "phoenix")
+
+print "Charlotte"
+pittsburgh_business_list = parse_by_city("Charlotte")
+print len(pittsburgh_business_list)
+pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
+print len(pittsburgh_reviews)
+sort_reviews_by_season(pittsburgh_reviews, "charlotte")
+
+print "Madison"
+pittsburgh_business_list = parse_by_city("Madison")
+print len(pittsburgh_business_list)
+pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
+print len(pittsburgh_reviews)
+sort_reviews_by_season(pittsburgh_reviews, "madison")
+
+print "Edinburgh"
+pittsburgh_business_list = parse_by_city("Edinburgh")
+print len(pittsburgh_business_list)
+pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
+print len(pittsburgh_reviews)
+sort_reviews_by_season(pittsburgh_reviews, "edinburgh")
+
+print "Mesa"
+pittsburgh_business_list = parse_by_city("Mesa")
+print len(pittsburgh_business_list)
+pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
+print len(pittsburgh_reviews)
+sort_reviews_by_season(pittsburgh_reviews, "mesa")
 
 
+pittsburgh_business_list = parse_by_city("Scottsdale")
+print len(pittsburgh_business_list)
+pittsburgh_reviews = get_reviews_from_business_list(pittsburgh_business_list)
+print len(pittsburgh_reviews)
+sort_reviews_by_season(pittsburgh_reviews, "scottsdale")
+'''
 
