d6c315477004aea096626d279e304e391c2b28f1
Colin Swan <colin.swan@pitt.edu>
2016-04-17 23:39:54 -0400

    Think I fixed a couple bugs in baseline

--- baseline.py
+++ baseline.py
@@ -5,20 +5,21 @@ class BaseLine:
   def __init__(self, reviews):
     # Determine which season occurs most often in training data
     # and use that season as guess for all test reviews.
-	counts = {"winter": 0, "spring": 0, "summer": 0, "fall": 0}
-	for review in reviews:
-		season = review[0]
-		if (season == "winter"):
-			counts["winter"] += 1
-		elif (season == "spring"):
-			counts["spring"] += 1
-		elif (season == "summer"):
-			counts["summer"] += 1
-		elif (season == "fall"):
-			counts["fall"] += 1
+    counts = {"winter": 0, "spring": 0, "summer": 0, "fall": 0}
+    for review in reviews:
+      season = review[0]
+      if (season == "winter"):
+        counts["winter"] += 1
+      elif (season == "spring"):
+        counts["spring"] += 1
+      elif (season == "summer"):
+        counts["summer"] += 1
+      elif (season == "fall"):
+        counts["fall"] += 1
 
-	self.most_common_season = max(counts.iteritems(), key=operator.itemgetter(1))[0]
-	return
+    common_count, common_season = max((x, y) for y, x in counts.items())
+    self.most_common_season = common_season
+    return
 
   # Classify collection of sentences in parsed_review_sentences (which should be
   # a list of parsed/tokenized sentences for a single review). Return the
