122b6abac03dba7e83d95470ce2833f42e2489c3
Colin Swan <colin.swan@pitt.edu>
2016-04-27 20:10:22 -0400

    Tweaked parameters for the RFE feature selector and vector's Ngram size.

--- logreg.py
+++ logreg.py
@@ -6,6 +6,7 @@ categorizing reviews into seasons.
 """
 import json
 import numpy as np
+import stop_words
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
@@ -23,12 +24,13 @@ class LogReg:
   def __init__(
       self,
       reviews,
-      vectorizer = TfidfVectorizer(max_df = 15, ngram_range = (1, 2)),
+      vectorizer = TfidfVectorizer(stop_words = 'english', max_df = 1,
+        ngram_range = (1, 2)),
       model = LogisticRegression()
       ):
     self.model = model
     self.vectorizer = vectorizer
-    self.selector = RFE(self.model, verbose = 100)
+    self.selector = RFE(self.model, step = 100, verbose = 100)
 
     corpus = []
     labels = []
@@ -44,42 +46,13 @@ class LogReg:
     X = self.vectorizer.fit_transform(self.corpus)
     self.feature_names = self.vectorizer.get_feature_names()
     y = self.labels
-    print(self.feature_names)
+    for string in self.feature_names:
+      print(string.encode("ascii", 'ignore'))
 
     #Training the model
     X_new = self.selector.fit_transform(X, self.labels)
     self.model.fit(X_new, self.labels)
 
-
-  """
-  Trains up the vectorizer and model with the given reviews.
-  """
-  """
-  def train(self, reviews):
-    #Vectorizing using TfidfVectorizer, which takes a count of all the words
-    #and then does some extra work to eliminate ones that are too common among
-    #all labels to be worth while.
-
-    #adding to the corpus and to the labels for this object
-    for review in reviews:
-      self.corpus += [review[1]["text"]]
-      self.labels += [review[0]]
-
-    X = self.vectorizer.fit_transform(self.corpus)
-    self.feature_names = self.vectorizer.get_feature_names()
-    self.feature_names = np.asarray(feature_names)
-    y = self.labels
-
-    #Training the model
-    X_new = SelectKBest(chi2, k=100).fit_transform(X, y)
-    self.model.fit(X_new, y)
-    """
-
-  """
-  Classifies all the test data given, and returns a list of the
-  classifications, which each index corresponding to the index of a review in
-  the given test data.
-  """
   def classify_all(self, all_test_data):
     test_corpus = []
     y = []
